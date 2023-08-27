f10882fd9ab458fc4632299218104cef1e910055
Kwazar374 <66804584+Kwazar374@users.noreply.github.com>
2023-08-27 16:47:49 +0200

    RealPython OOP Course p. 1

---
+++ RealPython/OOP_course/Intro_to_OOP_in_Python/creating_classes.py
@@ -0,0 +1,36 @@
+class Dog:
+    species = 'mammal'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def description(self):
+        return '{} is {} years old'.format(self.name, self.age)
+    
+    def speak(self, sound):
+        return '{} says {}'.format(self.name, sound)
+    
+    def birthday(self):
+        self.age += 1
+
+""" philo = Dog('Philo', 5)
+mikey = Dog('Mikey', 6)
+
+mikey.age = 7
+philo.species = 'mouse'
+
+print('{} is {} and {} is {}'.format(philo.name, philo.age, mikey.name, mikey.age))
+
+if philo.species == 'mammal':
+    print('{} is a {}'.format(philo.name, philo.species)) """
+
+mikey = Dog('Mikey', 6)
+
+print(mikey.description())
+
+print(mikey.speak('Gruff, gruff!'))
+
+mikey.birthday()
+
+print(mikey.description())
