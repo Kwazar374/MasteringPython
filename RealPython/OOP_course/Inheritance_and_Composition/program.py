d2c32b9a67b96cd9db7701cc0f855b354c15686b
Kwazar374 <66804584+Kwazar374@users.noreply.github.com>
2023-09-03 00:46:27 +0200

    RealPython Inheritance and Composition Course [Final version of App implemented with Strategy Pattern]

--- RealPython/OOP_course/Inheritance_and_Composition/program.py
+++ RealPython/OOP_course/Inheritance_and_Composition/program.py
@@ -1,24 +1,15 @@
-import hr
-import employees
-import productivity
+from hr import PayrollSystem, HourlyPolicy
+from productivity import ProductivitySystem
+from employees import EmployeeDatabase
 
-manager = employees.Manager(1, 'John Smith', 1500)
-secretary = employees.Secretary(2, 'Jane Doe', 1200)
-sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
-factory_worker = employees.FactoryWorker(4, 'Pete Peterson', 40, 15)
-temporary_secretary = employees.TemporarySecretary(5, 'Robin Williams', 40, 9)
+productivity_system = ProductivitySystem()
+payroll_system = PayrollSystem()
+employee_database = EmployeeDatabase()
 
-employees = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-    temporary_secretary 
-]
+employees = employee_database.employees()
 
-productivity_system = productivity.ProductivitySystem()
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
+
 productivity_system.track(employees, 40)
-
-payroll_system = hr.PayrollSystem()
 payroll_system.calculate_payroll(employees)
-
